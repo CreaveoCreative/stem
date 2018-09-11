a9f1000f1fddf7282d8d62410a09dd5867b37a8b
Cally <cartrf@live.com>
2018-09-11 22:05:02 +0100

    First commit, let's move forward

---
+++ Readme.txt
@@ -0,0 +1,11 @@
+Hi,
+
+Things you might want to take note of...
+In this work,
+> Bootstrap 4 and Material Design Layout
+> Angular Js routing is what is used for navigating pages
+> Different views are in the 'views' folder
+> Script for routing is in the js/controller.js file
+> index.html is what is holding the navigation and footer
+
+Well done.
---
+++ js/controller.js
@@ -0,0 +1,46 @@
+//////////////////////////////////////////////////////////////
+
+var stemHub = angular.module('stem', ['ngRoute']);
+
+stemHub.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/home.html'
+            })
+            .when('/steming', {
+                templateUrl: 'views/steming.html'
+            })
+            .when('/donate', {
+                templateUrl: 'views/donate.html'
+            })
+            .when('/stemstars', {
+                templateUrl: 'views/stemstars.html'
+            })
+            .when('/program', {
+                templateUrl: 'views/programs.html'
+            })
+            .when('/stemspire', {
+                templateUrl: 'views/stemspire.html'
+            })
+            .when('/about', {
+                templateUrl: 'views/about.html'
+            })
+            .when('/contact', {
+                templateUrl: 'views/contact.html'
+            })
+            .when('/volunteer', {
+                templateUrl: 'views/volunteer.html'
+            })
+            .when('/event', {
+                templateUrl: 'views/event.html'
+            })
+            .otherwise({ redirectTo: '/' });
+
+            $locationProvider.html5Mode(true).hashPrefix('!');
+    }]);
+
+stemHub.controller('stemController', function ($scope) {
+        var self = this;
+
+});
